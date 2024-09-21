e646602d83806930239f9982d8b3bb4afe8792d4
Crack8754 <puyadmitriy98@gmail.com>
2024-09-21 11:09:31 +0300

    Create Лабораторна робота №1.js

---
+++ Лабораторна робота №1.js
@@ -0,0 +1,89 @@
+//1. x8 за три операції
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * a;
+const c = b * b;
+console.log(c);
+
+//2. x17за п’ять операції 
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * x;
+const c = b * b;
+const d = c * c;
+const e = d * b;
+console.log(e);
+
+//3. x10 за чотири операції 
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * a;
+const c = b * a;
+const d = c * a;
+console.log(d);
+
+//4. x13 за п’ять операцій 
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * x;
+const c = b * b;
+const d = c * b;
+const e = d * b;
+console.log(e);
+
+//5. x21 за шість операції
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * a;
+const c = b * b;
+const d = c * c;
+const e = d * x;
+const f = e * x;
+console.log(f);
+
+//6. x15 за п’ять операцій 
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * x;
+const c = b * b;
+const d = c * c;
+const e = d * b;
+console.log(e);
+
+//7. x28 за шість операції 
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * a;
+const c = b * b;
+const d = c * c;
+const e = d * x;
+const f = e * e;
+console.log(f);
+
+//8. x64 за шість операції 
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * a;
+const c = b * b;
+const d = c * c;
+const e = d * d;
+const f = e * e;
+console.log(f);
+
+//9. x20 за п’ять операцій 
+
+const x = parseInt(prompt("Input x"));
+const a = x * x;
+const b = a * a;
+const c = b * b;
+const d = c * a;
+const e = d * d;
+console.log(e); 
